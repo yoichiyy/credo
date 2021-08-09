a7d58ac7b995d730aa77dac965564fe1ba341095
yoichiyy <yoichiyy@gmail.com>
2021-08-09 10:40:41 +0900

    insert Youtube Link

--- lib/constants/movie_links_constants.dart
+++ lib/constants/movie_links_constants.dart
@@ -1,11 +1,49 @@
+// class MorningVideosLinks {
+//   static getVideoLinks(value) {
+//     switch (value) {
+//       case 1:
+//         return "https://youtu.be/qz1pn-diGkU";
+//         break;
+//       case 2:
+//         return "https://youtu.be/26H0T4oXtxg";
+//         break;
+//     }
+//   }
+// }
+
+// class EveingVideosLinks {
+//   static getVideoLinks(value) {
+//     switch (value) {
+//       case 1:
+//         return "https://youtu.be/26H0T4oXtxg";
+//         break;
+//       case 2:
+//         return "https://youtu.be/qz1pn-diGkU";
+//         break;
+//     }
+//   }
+// }
+
 class MorningVideosLinks {
   static getVideoLinks(value) {
     switch (value) {
       case 1:
         return "https://youtu.be/qz1pn-diGkU";
         break;
-      case 2:
+        case2:
         return "https://youtu.be/26H0T4oXtxg";
+        break;
+      case 3:
+        return "https://youtu.be/uHq5jApdEho";
+        break;
+        case4:
+        return "https://youtu.be/UOYESAELDNM";
+        break;
+      case 5:
+        return "https://youtu.be/q6LyL4-Z4bA";
+        break;
+        case6:
+        return "https://youtu.be/jcNOXfqm-2M";
         break;
     }
   }
@@ -19,6 +57,18 @@ class EveingVideosLinks {
         break;
       case 2:
         return "https://youtu.be/qz1pn-diGkU";
+        break;
+      case 3:
+        return "https://youtu.be/ZXoAbalG9J0";
+        break;
+        case4:
+        return "https://youtu.be/ckgNbGL0X2s";
+        break;
+      case 5:
+        return "https://youtu.be/l2q_HwGLkrY";
+        break;
+        case6:
+        return "https://youtu.be/BRt32_CH45A";
         break;
     }
   }
