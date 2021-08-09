472b0120d544c55f9752338108ae639b50cdb1e7
Rohit Agarwal <rohitagarwal17800@gmail.com>
2021-08-09 10:05:52 +0530

    changes rectified

--- lib/constants/movie_links_constants.dart
+++ lib/constants/movie_links_constants.dart
@@ -1,48 +1,22 @@
-// class MorningVideosLinks {
-//   static getVideoLinks(value) {
-//     switch (value) {
-//       case 1:
-//         return "https://youtu.be/qz1pn-diGkU";
-//         break;
-//       case 2:
-//         return "https://youtu.be/26H0T4oXtxg";
-//         break;
-//     }
-//   }
-// }
-
-// class EveingVideosLinks {
-//   static getVideoLinks(value) {
-//     switch (value) {
-//       case 1:
-//         return "https://youtu.be/26H0T4oXtxg";
-//         break;
-//       case 2:
-//         return "https://youtu.be/qz1pn-diGkU";
-//         break;
-//     }
-//   }
-// }
-
 class MorningVideosLinks {
   static getVideoLinks(value) {
     switch (value) {
       case 1:
         return "https://youtu.be/qz1pn-diGkU";
         break;
-        case2:
+      case 2:
         return "https://youtu.be/26H0T4oXtxg";
         break;
       case 3:
         return "https://youtu.be/uHq5jApdEho";
         break;
-        case4:
+      case 4:
         return "https://youtu.be/UOYESAELDNM";
         break;
       case 5:
         return "https://youtu.be/q6LyL4-Z4bA";
         break;
-        case6:
+      case 6:
         return "https://youtu.be/jcNOXfqm-2M";
         break;
     }
@@ -61,13 +35,13 @@ class EveingVideosLinks {
       case 3:
         return "https://youtu.be/ZXoAbalG9J0";
         break;
-        case4:
+      case 4:
         return "https://youtu.be/ckgNbGL0X2s";
         break;
       case 5:
         return "https://youtu.be/l2q_HwGLkrY";
         break;
-        case6:
+      case 6:
         return "https://youtu.be/BRt32_CH45A";
         break;
     }
